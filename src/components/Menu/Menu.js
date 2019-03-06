e83a6046b9ef39612d50da19bda2278e22309bb9
Kendry Grullon <kengrullon@gmail.com>
2019-03-06 14:28:33 -0400

    Added a divition for the cases in the bio component. Adding the menu.

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <div>
+      <ul>
+        <li>Personal Info</li>
+        <li>Work Experience</li>
+        <li>Education</li>
+        <li>Contact Info</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu
--- src/containers/Bio/Bio.js
+++ src/containers/Bio/Bio.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 import Typography from "@material-ui/core/Typography";
+import Menu from "../../components/Menu/Menu";
+
+import "./Bio.css";
 
 export default class Bio extends Component {
   render() {
     return (
-      <>
-        <Typography variant="display1" align="right" gutterBottom>
-          Kendry Alexander Grullon
-        </Typography>
-      </>
+      <div className="Bio">
+        <div className="Menu">
+          <Menu />
+        </div>
+        <div className="Info">
+          <Typography variant="display1" align="right" gutterBottom>
+            Kendry Alexander Grullon
+          </Typography>
+        </div>
+      </div>
     )
   }
 }
