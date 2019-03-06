459f232f82c986543c005ffc44e035149ffc62f0
Kendry Grullon <kengrullon@gmail.com>
2019-03-06 14:08:14 -0400

    Added the Bio component, going to divide it in two.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import Typography from "@material-ui/core/Typography";
 import Toolbar from "./components/Toolbar/Toolbar";
-import './App.css';
+import Bio from "./containers/Bio/Bio";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Toolbar />
-        <Typography variant="display1" align="right" gutterBottom>
-          Kendry Alexander Grullon
-        </Typography>
-        <Route path="/bio" render={() => (<div>Hello</div>)}/>
+        <Route path="/bio" component={Bio} />
       </div>
     );
   }
---
+++ src/containers/Bio/Bio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Typography from "@material-ui/core/Typography";
+
+export default class Bio extends Component {
+  render() {
+    return (
+      <>
+        <Typography variant="display1" align="right" gutterBottom>
+          Kendry Alexander Grullon
+        </Typography>
+      </>
+    )
+  }
+}
