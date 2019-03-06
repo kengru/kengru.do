16994d0de58ed72ad0ccde8bf93e3a30a2ddb551
Kendry Grullon <kengrullon@gmail.com>
2019-03-06 15:27:56 -0400

    Adding list from material-ui.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import "./Menu.css";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  },
+});
+
 const Menu = props => {
-  console.log(props);
+  const { classes } = props;
+
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to={`${props.match.path}/info`}>Personal Info</NavLink>
-        </li>
-        <li>Work Experience</li>
-        <li>Education</li>
-        <li>Contact Info</li>
-      </ul>
+    <div className={classes.root}>
+      <List component="nav" >
+        <NavLink to={`${props.match.path}/info`}>
+          <ListItem button>
+            <ListItemText primary="Personal Info" />
+          </ListItem>
+        </NavLink>
+        <ListItem button>
+          <ListItemText primary="Work Experience" />
+        </ListItem>
+      </List>
+      <List component="nav">
+        <ListItem button>
+          <ListItemText primary="Education" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Contact Info" />
+        </ListItem>
+      </List>
     </div>
   );
 };
 
-export default Menu;
+export default withStyles(styles)(Menu);
