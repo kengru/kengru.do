02c904124fe73415dad41623a869fddc630f83b7
Kendry Grullon <kengrullon@gmail.com>
2019-03-07 08:31:48 -0400

    Implemented the P5Wrapper, finding an efficient way to have multiple sketches.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Toolbar from "./components/Toolbar/Toolbar";
 import Bio from "./containers/Bio/Bio";
+import P5Wrapper from "./components/P5Wrapper/P5Wrapper";
 import "./App.css";
 
 class App extends Component {
+  onSetAppState = (newState, cb) => this.setState(newState, cb);
+
   render() {
     return (
       <div className="App">
         <Toolbar />
-        <Route path="/bio" component={Bio} /> 
-        <Route path="/projects" component={Bio} /> 
-        <Route path="/nature" component={Bio} /> 
-        <Route path="/challenges" component={Bio} /> 
+        <Switch>
+          <Route path="/bio" component={Bio} />
+          <Route path="/projects" component={Bio} />
+          <Route path="/nature" component={Bio} />
+          <Route
+            path="/challenges"
+            render={() => (
+              <P5Wrapper
+                p5Props={{ slider: 100 }}
+                onSetAppState={this.onSetAppState}
+              />
+            )}
+          />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/P5Wrapper/P5Wrapper.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import sketch1 from "./sketch1";
+
+export default class P5Wrapper extends Component {
+  static propTypes = {
+    p5Props: PropTypes.object.isRequired,
+    onSetAppState: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.canvas1 = new window.p5(sketch1, "canvas1-container");
+    this.canvas1.props = this.props.p5Props;
+    this.canvas1.onSetAppState = this.props.onSetAppState;
+  }
+
+  shouldComponentUpdate(nextProps) {
+    this.canvas1.props = nextProps.p5Props;
+    return false;
+  }
+
+  componentWillUnmount() {
+    this.canvas1.remove();
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          id="canvas2-container"
+          style={{ width: "100%", textAlign: "center" }}
+        />
+      </>
+    );
+  }
+}
