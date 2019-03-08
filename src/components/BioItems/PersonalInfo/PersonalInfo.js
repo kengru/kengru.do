be933e63c71000052874e97fdf88cb28f0f45d77
Kendry Grullon <kengrullon@gmail.com>
2019-03-08 11:28:47 -0400

    Fixing last commit, sensitive case on a file.

--- src/components/BioItems/PersonalInfo/PersonalInfo.js
+++ src/components/BioItems/PersonalInfo/PersonalInfo.js
@@ -3,7 +3,7 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
-import "./personalInfo.css";
+import "./PersonalInfo.css";
 
 const personalInfo = () => {
   return (
