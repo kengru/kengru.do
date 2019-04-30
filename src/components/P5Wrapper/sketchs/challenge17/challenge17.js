5fc60805ab184bfe15901bf5603eda0a3c247800
Kendry Grullon <kengrullon@gmail.com>
2019-04-30 16:31:12 -0400

    Halfway on challenge 17.

--- src/components/P5Wrapper/sketchs/challenge17/challenge17.js
+++ src/components/P5Wrapper/sketchs/challenge17/challenge17.js
@@ -1,65 +1,90 @@
-let cols, rows;
-let w = 1800;
-let h = 1000;
-let scale = 10;
-let terrain = [];
-let flying = 0;
-// TODO: a bird flying by.
-
+let tree;
+let max_dist = 10;
+let min_dist = 10;
 
 export default function(s) {
   s.props = {};
   s.onSetAppState = () => {};
 
   s.setup = function() {
-    s.createCanvas(800, 400, s.WEBGL);
-    cols = w / scale;
-    rows = h / scale;
-
-    let yoff = 0;
-    for (let y = 0; y < rows; y++) {
-      let xoff = 0;
-      terrain[y] = [];
-      for (let x = 0; x < cols; x++) {
-        terrain[y][x] = s.map(s.noise(yoff, xoff), 0, 1, -50, 50);
-        xoff += 0.2;
-      }
-      yoff += 0.2;
-    }
+    s.createCanvas(600, 400);
+    tree = new Tree();
   };
 
   s.draw = function() {
-    s.ambientLight(150);
-    s.lights();
-    s.smooth();
-
-    flying -= 0.1;
-    let yoff = flying;
-    for (let y = 0; y < rows; y++) {
-      let xoff = 0;
-      terrain[y] = [];
-      for (let x = 0; x < cols; x++) {
-        terrain[y][x] = s.map(s.noise(yoff, xoff), 0, 1, -40, 40);
-        xoff += 0.1;
-      }
-      yoff += 0.1;
-    }
-
     s.background(255);
-    s.noStroke();
-    s.fill(20, 140, 50, 100);
-
-    s.rotateX(s.PI/3);
-    s.translate(-w/2, -h/2);
-
-    for (let y = 0; y < rows-1; y++) {
-      s.beginShape(s.TRIANGLE_STRIP);
-      // s.texture(img);
-      for (let x = 0; x < cols; x++) {
-        s.vertex(x * scale, y * scale, terrain[y][x]);
-        s.vertex(x * scale, (y+1) * scale, terrain[y][x]);
-      }
-      s.endShape();
-    }
+    tree.show();
   };
+
+  class Tree {
+    constructor() {
+      this.leaves = [];
+      this.branches = [];
+      for (var i = 0; i < 100; i++) {
+        this.leaves.push(new Leaf());
+      }
+      let position = s.createVector(s.width/2, s.height);
+      let dir = s.createVector(0, -1);
+      let root = new Branch(null, position, dir);
+      this.branches.push(root);
+
+      let current = root;
+      let found = false;
+      while (!found) {
+        for (let i = 0; i < this.leaves.length; i++) {
+          let d = current.pos.dist(this.leaves[i].pos);
+          if (d < max_dist) {
+            found = true;
+          }
+        }
+        if (!found) {
+          let branch = current.next();
+          current = branch;
+          this.branches.push(current);
+        }
+      }
+    }
+
+    show() {
+      for (let i = 0; i < this.leaves.length; i++) {
+        this.leaves[i].show();
+      }
+      for (let i = 0; i < this.branches.length; i++) {
+        this.branches[i].show();
+      }
+    }
+  }
+
+  class Branch {
+    constructor(parent, pos, dir) {
+      this.parent = parent;
+      this.pos = pos;
+      this.dir = dir;
+    }
+
+    next() {
+      let nextPos = this.pos.add(this.dir);
+      let nextBranch = new Branch(this, nextPos, this.dir.copy());
+      return nextBranch;
+    }
+
+    show() {
+      if (this.parent != null) {
+        s.stroke(0);
+        s.line(this.pos.x, this.pos.y, this.parent.pos.x, this.parent.pos.y);
+      }
+    }
+  }
+
+  class Leaf {
+    constructor() {
+      this.pos = s.createVector(s.random(s.width), s.random(s.height - 100));
+    }
+
+    show() {
+      s.fill(0);
+      s.noStroke();
+      s.ellipse(this.pos.x, this.pos.y, 4, 4);
+    }
+  }
 }
