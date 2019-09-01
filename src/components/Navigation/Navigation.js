dc7f4c0fe0d9e62b2a7643857b6465e58ceb9ed2
kengru <kengrullon@gmail.com>
2019-09-01 17:07:28 -0400

    Added navbar burger toggle.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,6 +5,13 @@ import "rbx/index.css";
 
 import "./Navigation.css";
 
+function nItem() {
+  var menu = document.querySelector(".navbar-menu");
+  var burger = document.querySelector(".navbar-burger");
+  menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
+}
+
 const Navigation = () => {
   return (
     <Section>
@@ -23,27 +30,32 @@ const Navigation = () => {
           </Navbar.Brand>
           <Navbar.Menu>
             <Navbar.Segment align="end">
-              <Navbar.Item as="div">
+              <Navbar.Item as="div" onClick={nItem}>
                 <NavLink activeClassName="navActive" exact to="/">
                   Home
                 </NavLink>
               </Navbar.Item>
-              <Navbar.Item as="div">
+              <Navbar.Item as="div" onClick={nItem}>
                 <NavLink activeClassName="navActive" to="/projects">
                   Projects
                 </NavLink>
               </Navbar.Item>
-              <Navbar.Item as="div">
+              <Navbar.Item as="div" onClick={nItem}>
+                <NavLink activeClassName="navActive" to="/games">
+                  Games
+                </NavLink>
+              </Navbar.Item>
+              <Navbar.Item as="div" onClick={nItem}>
                 <NavLink activeClassName="navActive" to="/renders">
                   3D Modeling
                 </NavLink>
               </Navbar.Item>
-              <Navbar.Item as="div">
+              <Navbar.Item as="div" className="is-hidden-mobile">
                 <NavLink activeClassName="navActive" to="/challenges">
                   Challenges
                 </NavLink>
               </Navbar.Item>
-              <Navbar.Item as="div">
+              <Navbar.Item as="div" className="is-hidden-mobile">
                 <NavLink activeClassName="navActive" to="/nature">
                   Nature of Code
                 </NavLink>
@@ -54,6 +66,7 @@ const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://github.com/kengru"
+                onClick={nItem}
               >
                 <i className="fab fa-github" />
               </Navbar.Item>
@@ -61,6 +74,7 @@ const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://twitter.com/kxngru"
+                onClick={nItem}
               >
                 <i className="fab fa-twitter" />
               </Navbar.Item>
@@ -68,6 +82,7 @@ const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.linkedin.com/in/kengru/"
+                onClick={nItem}
               >
                 <i className="fab fa-linkedin" />
               </Navbar.Item>
@@ -75,6 +90,7 @@ const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://medium.com/@kengru"
+                onClick={nItem}
               >
                 <i className="fab fa-medium-m" />
               </Navbar.Item>
@@ -83,6 +99,7 @@ const Navigation = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 href={`https://firebasestorage.googleapis.com/v0/b/kengru-do.appspot.com/o/public%2FKGrullon-Resume.pdf?alt=media&token=c884163a-356c-437e-9df5-5ca29c8b96f5`}
+                onClick={nItem}
               >
                 <Button.Group>
                   <Button color="link">Resume</Button>
