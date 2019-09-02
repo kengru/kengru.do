9061c89ea99142206227ac4b64273cb71117c13a
kengru <kengrullon@gmail.com>
2019-09-01 23:54:30 -0400

    Added css to the menu page of games.

--- src/components/GamesComponents/GamesMenu/GamesMenu.jsx
+++ src/components/GamesComponents/GamesMenu/GamesMenu.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Column, Title, Card, Media, Content, Tag } from "rbx";
 
 const GamesMenu = () => {
   return (
-    <div>
-      Games Menu
-    </div>
+    <React.Fragment>
+      <Title>Created Games</Title>
+      <Column key={item.name} size={3}>
+        <Card>
+          <Card.Image>
+            <Image.Container size={128}>
+              <Image src={item.image} />
+            </Image.Container>
+          </Card.Image>
+          <Card.Content>
+            <Media>
+              <Media.Item>
+                <Title
+                  as="a"
+                  size={4}
+                  href={item.vc}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.name} &nbsp;
+                  <i className="fas fa-link" />
+                </Title>
+              </Media.Item>
+            </Media>
+            <Content>{item.description}</Content>
+          </Card.Content>
+          <Card.Footer>
+            <Card.Footer.Item
+              as="a"
+              href={item.vc}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source Code
+            </Card.Footer.Item>
+            {item.link ? (
+              <Card.Footer.Item
+                as="a"
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live link
+              </Card.Footer.Item>
+            ) : null}
+          </Card.Footer>
+          <Card.Footer  style={{ display: "block" }}>
+            {item.tools.map(skill => (
+              <Tag key={skill}>{skill}</Tag>
+            ))}
+          </Card.Footer>
+        </Card>
+      </Column>
+    </React.Fragment>
   )
 }
 
--- src/containers/Games/Games.jsx
+++ src/containers/Games/Games.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import { Column, Title, Content, Hero } from "rbx";
+import { Hero } from "rbx";
+
+import GameMenu from "../../components/GamesComponents/GamesMenu/GamesMenu";
 
 import RouteWithSubs from "../../helpers/routeWithSubs";
 import routes from "../../components/GamesComponents/routes";
-import GameMenu from "../../components/GamesComponents/GamesMenu/GamesMenu";
+
+import "./Games.css"
 
 class Games extends Component {
   render() {
     return (
-      <Hero.Body>
+      <Hero.Body className="gameBody">
         <Switch>
           <Route exact path="/games" component={GameMenu} />
           {routes.map((route, i) => (
