eb7f8d7927cb8d058963a7df9c7218a2ae9011c8
kengru <kengrullon@gmail.com>
2020-07-12 01:38:39 -0400

    Added routing and nav items.

--- kengrudo/src/components/SideInfo.tsx
+++ kengrudo/src/components/SideInfo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import { StyleSheet, css } from "aphrodite/no-important";
 
 import { rales } from "../fonts/fonts";
@@ -12,7 +13,7 @@ const styles = StyleSheet.create({
     paddingLeft: "2em",
     flexDirection: "column",
     justifyContent: "center",
-    fontSize: "calc(3px + 2vmin)",
+    fontSize: "calc(3px + 2vmin)"
   },
   logo: {
     alignSelf: "flex-start"
@@ -23,18 +24,66 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: "calc(9px + 1vmin)"
+  },
+  navList: {
+    display: "flex",
+    flexDirection: "column",
+    listStyle: "none",
+    padding: "0em"
+  },
+  listItem: {
+    color: "#07020D",
+    width: "fit-content",
+    textDecoration: "none",
+    padding: "0.4em 0em 0.4em 0em",
+    transform: "perspective(1px) translateZ(0)",
+    transitionProperty: "transform",
+    transitionDuration: "0.3s",
+    ":hover": {
+      transform: "translateX(8px)"
+    }
+  },
+  listItemActive: {
+    fontWeight: "bold"
   }
 });
 
 function SideInfo() {
   return (
     <div className={css(styles.side)}>
-      <img className={css(styles.logo)} src={logo} alt="logo" />
+      <Link to="/">
+        <img className={css(styles.logo)} src={logo} alt="logo" />
+      </Link>
       <div className={css(styles.name)}>Kendry Alexander Grullón</div>
       <p className={css(styles.text)}>
-        Programming things, mainly using javascript
+        Programming things, mainly using javascript.
         <br />I have developed a thing for 3D.
       </p>
+      <nav>
+        <ul className={css(styles.navList)}>
+          <NavLink
+            activeClassName={css(styles.listItemActive)}
+            className={css(styles.listItem)}
+            to={"/work"}
+          >
+            work
+          </NavLink>
+          <NavLink
+            activeClassName={css(styles.listItemActive)}
+            className={css(styles.listItem)}
+            to={"/projects"}
+          >
+            projects
+          </NavLink>
+          <NavLink
+            activeClassName={css(styles.listItemActive)}
+            className={css(styles.listItem)}
+            to={"/skills"}
+          >
+            skills
+          </NavLink>
+        </ul>
+      </nav>
     </div>
   );
 }
--- kengrudo/src/index.tsx
+++ kengrudo/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { App } from "./containers/App";
+import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+
+import { App } from "./containers/App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
