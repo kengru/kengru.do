1869e064593b864324ff2992fd28d5bd4c235caa
Kendry Grullon <kengrullon@gmail.com>
2020-11-19 16:30:53 -0400

    Migrated the new version to typescript. Type to redeploy.

--- kengrudo/README.md
+++
@@ -1,4 +0,0 @@
-# kengru.do
-![ci-build](https://travis-ci.org/kengru/kengru.do.svg?branch=master)
-
-My personal webpage.
--- src/store/actions/index.js
+++
@@ -1,5 +0,0 @@
-export { fetchWorkAsync } from "./bio";
-export { fetchProjectsAsync } from "./projects";
-export { fetchSketchAsync, fetchMenuAsync } from "./challenges";
-export { fetchMenuNatureAsync } from "./nature";
-export { fetchGamesAsync } from "./games";
