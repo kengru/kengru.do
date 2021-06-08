588d7c9f047c41564156bf68853ad4c37d7fbc17
kengru <kengrullon@gmail.com>
2021-06-08 17:45:09 -0400

    Changed toggle to look fine.

--- src/components/ToggleLang.tsx
+++ src/components/ToggleLang.tsx
@@ -9,15 +9,39 @@ type Languages = "English" | "Español";
 
 const styles = StyleSheet.create({
   container: {
-    fontSize: "calc(8px + 1vmin)",
+    alignItems: "center",
+    display: "flex",
+    fontSize: "calc(7px + 1vmin)",
     marginTop: "20px"
   },
-  language: {},
-  toggle: {
+  language: {
+    fontWeight: 700,
+    marginLeft: "10px"
+  },
+  toggleOut: {
+    backgroundColor: "rbg(128, 128, 128)",
+    border: "2px solid rgba(17, 103, 177, 0.44)",
+    borderRadius: "20px",
     height: "30px",
-    width: "60px",
-    border: "1px solid black",
-    borderRadius: "20px"
+    width: "60px"
+  },
+  toggleIn: {
+    // border: "1px solid black",
+    borderRadius: "100%",
+    height: "23px",
+    left: "5px",
+    position: "relative",
+    transition: "left 200ms",
+    top: "3px",
+    width: "23px"
+  },
+  left: {
+    backgroundColor: "#0066b2",
+    left: "5px"
+  },
+  right: {
+    backgroundColor: "#29AB87",
+    left: "32px"
   }
 });
 
@@ -37,8 +61,17 @@ export const ToggleLang = () => {
 
   return (
     <div className={css(styles.container)} onClick={onToggle}>
-      <div className={css(styles.toggle)}></div>
-      <span className={css(styles.language)}>{language}</span>
+      <div className={css(styles.toggleOut)}>
+        <div
+          className={css(
+            styles.toggleIn,
+            language === "English" ? styles.left : styles.right
+          )}
+        ></div>
+      </div>
+      <span className={css(styles.language)}>
+        {language.substring(0, 2).toUpperCase()}
+      </span>
     </div>
   );
 };
