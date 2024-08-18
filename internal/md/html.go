3aad73ecbdc9101b74efcd9186e0124de9fb29b8
Kendry Grullon <kengrullon@gmail.com>
2024-08-18 11:55:18 -0400

    making it stable.

--- internal/md/html.go
+++ internal/md/html.go
@@ -2,40 +2,22 @@ package md
 
 import (
 	"bufio"
-	"io"
 	"strings"
-	"text/template"
 )
 
-type FinalContent struct {
-	data []byte
-}
-
-func (fc FinalContent) Write(p []byte) (n int, err error) {
-	l := len(p)
-	newLine := []byte("\n")
-	fc.data = append(fc.data, p...)
-	fc.data = append(fc.data, newLine...)
-	return l, nil
-}
-
 func MDtoHTML(content string) {
-	final := FinalContent{}
+	final := ""
 	scn := bufio.NewScanner(strings.NewReader(content))
 	for scn.Scan() {
 		if len(scn.Text()) < 1 {
 			continue
 		}
 		if scn.Text()[0] == '#' {
-			heading(scn.Text(), final)
+			final += heading(scn.Text())
 		}
 	}
 }
 
-func heading(line string, w io.Writer) {
-	tmpl := template.New("templates/heading.tmpl")
-	err := tmpl.Execute(w, line)
-
-	// // sp := strings.Cut(line, " ")
-	return "<h1>"
+func heading(line string) string {
+	return "<h1>" + line + "</h1>"
 }
